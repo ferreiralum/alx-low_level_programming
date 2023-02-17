dc435228add66cd6b90f683fd2d4e47f91053b5e
ferreiralum <105362966+ferreiralum@users.noreply.github.com>
2023-02-17 14:46:16 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,22 +3,22 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all single digit numbers of base 10 starting from 0,
+ * Description: Prints all single digit numbers of base 10
+ * starting from 0,
  * followed by a new line, using the putchar function twice.
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = '0'; i <= '9'; i++)
-    {
-        putchar(i);
-    }
-
-    putchar('\n');
-
-    return 0;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
 }
+putchar('\n');
 
+return (0);
+}
